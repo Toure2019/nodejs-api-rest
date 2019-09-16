e7423ba6b3bf4edafe03ca34dde1d375993d58e3
Toure2019 <51404116+Toure2019@users.noreply.github.com>
2019-09-16 17:46:05 +0200

    modif sur la branche dev...

--- routes/usersCtrl.js
+++ routes/usersCtrl.js
@@ -2,6 +2,11 @@
 var bcrypt = require('bcrypt');
 var jwtUtils = require('../utils/jwt.utils');
 var models = require('../models');
+var asyncLib = require('async');
+
+// Constants
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PASSWORD_REGEX = /^(?=.*\d).{4,8}$/;
 
 // Routes
 module.exports = {
@@ -17,37 +22,65 @@ module.exports = {
         }
 
         // TODO: verify pseudo length, mail regex, password etc.
+        if (username.length >= 13 || username.length <= 4) {
+            return res.status(400).json({ 'error': 'Wrong username (must length 5-12)' });
+        }
 
-        models.User.findOne({
-            attributes: ['email'],
-            where: { email: email }
-        })
-        .then(function(userFound) {
-            if (!userFound) {
-                bcrypt.hash(password, 5, function(err, bcryptedPassword) {
-                    var newUser = models.User.create({
-                        email: email,
-                        username: username,
-                        password: bcryptedPassword,
-                        bio: bio,
-                        isAdmin: 0
-                    })
-                    .then(function(newUser) {
-                        return res.status(201).json({
-                            'userId': newUser.id 
-                        });
-                    })
-                    .catch(function(err) {
-                        return res.status(500).json({ 'error': 'Cannot add user' });
+        if (!EMAIL_REGEX.test(email)) {
+            return res.status(400).json({ 'error': 'Email is not valid' });
+        }
+
+        if (!PASSWORD_REGEX.test(password)) {
+            return res.status(400).json({ 'error': 'password invalid (must be lengtg 4-8 and include 1 number at'})
+        }
+
+        // La fonction ".waterfall" permet d'exécuter des fonctions en cascade
+        asyncLib.waterfall([
+            function(done) {
+                models.User.findOne({
+                    attributes: ['email'],
+                    where: {email: email}
+                })
+                .then(function(userFound) {
+                    done(null, userFound);
+                })
+                .catch(function(err) {
+                    return res.status(500).json({ 'erreur': 'Unable to verify user' });
+                })
+            },
+            function(userFound, done) {
+                if (!userFound) {
+                    bcrypt.hash(password, 5, function(err, bcryptedPassword) {
+                        done(null, userFound, bcryptedPassword);
                     });
-                });
-            } else {
-                return res.status(409).json({ 'error': 'user already exists !' });
+                } else {
+                    return res.status(409).json({ 'error': 'User already exist !' });
+                }
+            },
+            function(userFound, bcryptedPassword, done) {
+                var newUser = models.User.create({
+                    email: email,
+                    username: username,
+                    password: password,
+                    bio: bio,
+                    isAdmin: 0
+                })
+                .then(function(newUser) {
+                    done(newUser);
+                })
+                .catch(function(err) {
+                    return res.status(500).json({ 'error': 'Cannot add user !' });
+                })
             }
 
-        })
-        .catch(function(err) {
-            return res.status(500).json({ 'error': 'unable to verify user' });
+        ], function(newUser) {
+            if (newUser) {
+                return res.status(201).json({
+                    'userId': newUser.id
+                });
+            } else {
+                return res.status(500).json({ 'error': 'Cannot add user !!' });
+            }
         });
     },
      
@@ -61,29 +94,108 @@ module.exports = {
         }
 
         // TODO: Verify email regex & password length
+        asyncLib.waterfall([
+            function(done) {
+                models.User.findOne({
+                    where: {email: email}
+                })
+                .then(function(userFound) {
+                    done(null, userFound);
+                })
+                .catch(function(err) {
+                    return res.status(500).json({ 'error': 'unable to verify user' });
+                });
+            }, function(userFound, done) {
+                if (userFound) {
+                    bcrypt.compare(password, userFound.password, function(errBcrypt, resBcrypt) {
+                        done(null, userFound, resBcrypt);
+                    })
+                } else {
+                    res.status(404).json({ 'error': 'user not exists in DB :)' });
+                }
+            }, function(userFound, resBcrypt, done) {
+                if (resBcrypt) {
+                    done(userFound);
+                } else {
+                    res.status(403).json({ 'error': 'invalid password' });
+                }
+            }
+        ], function(userFound) {
+            if (userFound) {
+                return res.status(201).json({
+                    'userId': userFound.id,
+                    'token': jwtUtils.generateTokenForUser(userFound)
+                });
+            } else {
+                return res.status(500).json({ 'error': 'Cannont log on user' });
+            }
+        });
+    },
+    getUserProfile: function(req, res) {
+        // Get auth hearder
+        var headerAuth = req.headers['authorization'];
+        var userId = jwtUtils.getUserId(headerAuth);
+
+        if (userId < 0) 
+            return res.status(400).json({ 'error': 'Wrong token' });
 
         models.User.findOne({
-            // attributes: ['email'], /* On Supprime cette ligne car on veut sélectionner tous les attributs */
-            where: {email: email}
+            attributes: ['id', 'email', 'username', 'bio'],
+            where: {id: userId}
         })
-        .then(function(userFound) {
-            if (userFound) {
-                bcrypt.compare(password, userFound.password, function(errBcrypt, resBcrypt) {
-                    if (resBcrypt) {
-                        return res.status(200).json({
-                            'userId': userFound.id,
-                            'token': jwtUtils.generateTokenForUser(userFound)
-                        });
-                    } else {
-                        return res.status(403).json({ 'error': 'invalid password' });
-                    }
-                })
+        .then(function(user) {
+            if (user) {
+                res.status(201).json(user);
             } else {
-                return res.status(404).json({ 'error': 'User not exists in DB !' });
+                res.status(404).json({ 'error': 'User not found' }); 
             }
         })
         .catch(function(err) {
-            return res.status(500).json({ 'error': 'Unable to verify user' });
+            res.status(500).json({ 'error': 'Cannot fetch user' });
+        });
+    },
+
+    updateUserProfile: function(req, res) {
+        // Getting auth header
+        var headerAuth  = req.headers['authorization'];
+        var userId      = jwtUtils.getUserId(headerAuth);
+    
+        // Params
+        var bio = req.body.bio;
+    
+        asyncLib.waterfall([
+            function(done) {
+                models.User.findOne({
+                    attributes: ['id', 'bio'],
+                    where: { id: userId }
+                })
+                .then(function (userFound) {
+                    done(null, userFound);
+                })
+                .catch(function(err) {
+                    return res.status(500).json({ 'error': 'unable to verify user' });
+                });
+            },
+            function(userFound, done) {
+                if(userFound) {
+                    userFound.update({
+                        bio: (bio ? bio : userFound.bio)
+                    }).then(function() {
+                        done(userFound);
+                    }).catch(function(err) {
+                        res.status(500).json({ 'error': 'cannot update user' });
+                    });
+                } else {
+                    res.status(404).json({ 'error': 'user not found' });
+                }
+            },
+        ],  
+        function(userFound) {
+            if (userFound) {
+                return res.status(201).json(userFound);
+            } else {
+                return res.status(500).json({ 'error': 'cannot update user profile' });
+            }
         });
     }
 }
